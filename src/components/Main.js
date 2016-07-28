6e93101d168e102fd478e709be47a97f8236cbe4
peng <young2_peng@163.com>
2016-07-29 01:55:59 +0800

    add base style

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,16 +9,33 @@ let imgDatastmp = imgDatas.map((singleImgData) => {
 	singleImgData.imgURL = require(`../images/${singleImgData.fileName}`)
 	return singleImgData
 })
-console.log(imgDatastmp)
+
+class ImgFigure extends React.Component {
+	render() {
+		return (
+			<figure className="img-figure">
+				<img src={this.props.data.imgURL}
+					alt={this.props.data.title} />
+				<figcaption>
+					<h2>{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		)
+	}
+}
 
 class AppComponent extends React.Component {
   render() {
-  	
+  	let imgFigures = []
+  	imgDatastmp.forEach(value => {
+  		imgFigures.push(<ImgFigure data={value} />)
+  	})
+
     return (
       	<section className="stage">
       		<section className="img-sec">
+      			{imgFigures}
       			<nav className="controller-nav">
-      				
       			</nav>
       		</section>
       	</section>
